e5ea09c2a6f3cb0d12c39566563799e61fade214
Ivan <ivanzhuravlev9051995@gmail.com>
2023-01-04 18:18:11 +0300

    task2,4,6,8

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c) 
+{
+    Console.WriteLine("Это число является максимальным: " + a);
+}
+else if (b > a && b > c)
+{
+    Console.Write("Это число является максимальным: " + b);
+}
+else 
+{
+    Console.Write("Это число является максимальным: " + c);
+}
